f0a6a12ad0800831774313b021fd1e6f423e2f32
F1elle <vana030680@gmail.com>
2024-03-07 23:00:35 +0200

    Changed link

--- app/src/main/java/com/f1elle/prng/prng/ui/CoprScreen.kt
+++ app/src/main/java/com/f1elle/prng/prng/ui/CoprScreen.kt
@@ -7,28 +7,25 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.f1elle.prng.R
-import com.f1elle.prng.prng.ui.utils.appButton
 import com.f1elle.prng.theme.Background
 
 @Composable
 fun coprScreen(navController: NavController) {
     val uriHandler = LocalUriHandler.current
+    val git = "https://github.com/F1elle/PseudoRandomNumbersGenerator"
     LazyColumn(modifier = Modifier
         .fillMaxSize()
         .background(color = Background),
@@ -48,7 +45,7 @@ fun coprScreen(navController: NavController) {
             Icon(painter = painterResource(id = R.drawable.git),
                 contentDescription = "GitHub repo",
                 tint = Color.White,
-                modifier = Modifier.size(30.dp).clickable { uriHandler.openUri("")})
+                modifier = Modifier.size(30.dp).clickable { uriHandler.openUri(git)})
         }
     }
 }
